d775de3934f656c1784fb09d4fbe607016569763
h.laroussi <h.laroussi@oodrive.com>
2025-09-21 21:00:20 +0200

    feat: Get stories from JSONbin API instead

--- lib/data/stories_data.dart
+++ lib/data/stories_data.dart
@@ -5,10 +5,17 @@ import 'package:http/http.dart' as http;
 import '../models/story.dart';
 
 Future<List<Story>> loadStories() async {
-  final response = await http.get(Uri.parse('https://kidibook-api.onrender.com/stories'));
+  final response = await http.get(
+    Uri.parse('https://api.jsonbin.io/v3/b/68cfe54143b1c97be94a42da'),
+    headers: {
+      'X-Master-Key':
+          r'$2a$10$cHUzegI/Vs3lyG.ouom.4uIMYjBiC4AswTN3382LeWEP/1n0nGj7q',
+    },
+  );
 
   if (response.statusCode == 200) {
-    final List<dynamic> jsonList = json.decode(response.body);
+    final Map<String, dynamic> data = json.decode(response.body);
+    final List<dynamic> jsonList = data['record'];
     return jsonList.map((json) => Story.fromJson(json)).toList();
   } else {
     throw Exception('Failed to load stories');
